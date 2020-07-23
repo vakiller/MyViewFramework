34ebd302ae5ea9ac98421df28fce15388a89fe73
vakiller <vapartner1997@gmail.com>
2020-07-23 09:34:15 +0700

    Initial Commit

---
+++ MyViewFramwork/MyViewFramwork.h
@@ -0,0 +1,19 @@
+//
+//  MyViewFramwork.h
+//  MyViewFramwork
+//
+//  Created by Aiden Le on 7/23/20.
+//  Copyright © 2020 Aiden Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyViewFramwork.
+FOUNDATION_EXPORT double MyViewFramworkVersionNumber;
+
+//! Project version string for MyViewFramwork.
+FOUNDATION_EXPORT const unsigned char MyViewFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyViewFramwork/PublicHeader.h>
+
+
